853eec5c2cebdc7b8f483dd1730f2c9c564d15f4
Daniel Kuruc <daniel@kuruc.dev>
2021-07-03 12:53:22 +0200

    Ansible / Docker / Home: tweak config & disable web server logging

---
+++ ansible/roles/docker_container_home/files/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Source: https://github.com/bastienwirtz/homer/blob/main/entrypoint.sh
+
+# Ensure default assets are present.
+while true; do echo n; done | cp -Ri /www/default-assets/* /www/assets/ &> /dev/null
+
+# Ensure compatibility with previous version (config.yml was in the root directory)
+if [ -f "/www/config.yml" ]; then
+    yes n | cp -i /www/config.yml /www/assets/ &> /dev/null
+fi
+
+# Install default config if no one is available.
+yes n | cp -i /www/default-assets/config.yml.dist /www/assets/config.yml &> /dev/null
+
+chown -R $UID:$GID /www/assets
+exec su-exec $UID:$GID darkhttpd /www/ --log /dev/null --no-listing --port "$PORT"
