5f8959310e80d9a193f06fee4eba4fbc78ed6c88
Daniel Kuruc <daniel@kuruc.dev>
2021-07-04 19:10:15 +0200

    Ansible / Unraid / Prerequisites / Refactor hddtemp

--- ansible/roles/unraid_prerequisites/other/boot_hddtemp.sh
+++
@@ -1,7 +0,0 @@
-logger 'Configuring hddtemp'
-
-if [ -f "{{ unraid.homelab_dir }}/hddtemp/rc.hddtemp" ]
-then
-  fromdos < "{{ unraid.homelab_dir }}/hddtemp/rc.hddtemp" > /etc/rc.d/rc.hddtemp
-  chmod +x /etc/rc.d/rc.hddtemp
-fi
