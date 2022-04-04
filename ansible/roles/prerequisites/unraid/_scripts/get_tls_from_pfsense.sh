682d42d12a0ca55fa2c16ce4aca86fec15fc612f
Daniel Kuruc <daniel@kuruc.dev>
2022-04-04 23:12:55 +0200

    Bugfix generating PKCS#12 TLS certificate

--- ansible/roles/prerequisites/unraid/_scripts/get_tls_from_pfsense.sh
+++ ansible/roles/prerequisites/unraid/_scripts/get_tls_from_pfsense.sh
@@ -19,16 +19,21 @@ rm -f /tmp/pfsense-*.xml
 # https://gist.github.com/novemberborn/4eb91b0d166c27c2fcd4
 wget -O ./intermediate.pem https://letsencrypt.org/certs/lets-encrypt-r3.pem
 wget -O ./ca.pem https://letsencrypt.org/certs/isrg-root-x1-cross-signed.pem
+cat intermediate.pem ca.pem > chain.pem
+cat cert.pem chain.pem > fullchain.pem
 
-openssl pkcs12 -passout pass: -export \
+# https://letsencrypt.org/certificates/
+openssl pkcs12 -export \
+  -passout  pass:password \
   -out      ./certificate.p12 \
   -inkey    ./privkey.pem \
   -in       ./cert.pem \
-  -certfile ./intermediate.pem
+  -certfile ./chain.pem
 
-cat cert.pem ca.pem > fullchain.pem
-
-for fname in certificate.p12 cert.pem fullchain.pem intermediate.pem privkey.pem
+for fname in certificate.p12 cert.pem chain.pem fullchain.pem intermediate.pem privkey.pem
 do
   ln -rsfv ./${fname} "{{ unraid.tls_storage_dir }}/${fname}"
+
+  # https://docs.microsoft.com/en-us/answers/questions/613333/loading-certificatepfx-with-password-in-linux-does.html
+  chmod 755 ./${fname} "{{ unraid.tls_storage_dir }}/${fname}"  # !!!!!!!!!!!!!!!!!!!!!
 done
