c8f607314381b37d3ea429b10828b5efaef1ef1e
Daniel Kuruc <daniel@kuruc.dev>
2022-04-09 16:25:14 +0200

    Various small improvements

--- ansible/roles/prerequisites/unraid/_webhooks/webhook_handler.sh
+++ ansible/roles/prerequisites/unraid/_webhooks/webhook_handler.sh
@@ -6,10 +6,12 @@ fi
 
 readonly ACTION="${1}"
 
+{% raw %}
 if [[ "${ACTION}" == "tls" ]]
 then
   /usr/bin/get_tls_from_pfsense.sh
-  docker restart jellyfin &
-  docker restart traefik &
+  [[ "$(docker ps --filter 'name=^jellyfin$' --format '{{.Names}}' 1>/dev/null 2>&1)" == 'jellyfin' ]] && docker restart jellyfin &
+  [[ "$(docker ps --filter 'name=^traefik$' --format '{{.Names}}' 1>/dev/null 2>&1)" == 'traefik' ]] && docker restart traefik &
   /usr/bin/update_idrac_tls.sh
 fi
+{% endraw %}
