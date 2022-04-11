0227b01d81ba3d204c5304c2e071322cd39f72fb
Daniel Kuruc <daniel@kuruc.dev>
2022-04-11 20:58:30 +0200

    Extract 'unraid/scripts' from 'prerequisites/unraid'

--- ansible/roles/unraid/scripts/templates/update_idrac_tls.sh
+++ ansible/roles/unraid/scripts/templates/update_idrac_tls.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # https://www.dell.com/support/kbdoc/en-us/000120158/dell-poweredge-how-to-import-an-externally-created-custom-certificate-and-private-key-into-the-idrac
-racadm -r "${IDRAC_HOST}" -u "${IDRAC_USERNAME}" -p "${IDRAC_PASSWORD}" sslkeyupload  -t 1 -f ./privkey.pem
-racadm -r "${IDRAC_HOST}" -u "${IDRAC_USERNAME}" -p "${IDRAC_PASSWORD}" sslcertupload -t 1 -f ./cert.pem
+racadm -r "${IDRAC_HOST}" -u "${IDRAC_USERNAME}" -p "${IDRAC_PASSWORD}" sslkeyupload  -t 1 -f '{{ unraid.tls_storage_dir }}'/privkey.pem
+racadm -r "${IDRAC_HOST}" -u "${IDRAC_USERNAME}" -p "${IDRAC_PASSWORD}" sslcertupload -t 1 -f '{{ unraid.tls_storage_dir }}'/cert.pem
 
 racadm -r "${IDRAC_HOST}" -u "${IDRAC_USERNAME}" -p "${IDRAC_PASSWORD}" racreset soft
 sleep 3m
